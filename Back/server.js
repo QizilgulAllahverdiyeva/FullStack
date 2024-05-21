7e279d7904db9f51a430b04828a88b3c975f53ef
QizilgulAllahverdiyeva <allahverdiyeva.qizilgul2003@gmail.com>
2024-05-21 08:51:38 +0400

    FullTask

---
+++ Back/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json())
